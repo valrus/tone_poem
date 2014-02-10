801626689b00577bd48a48d8feebba7baff484d5
Ian McCowan <imccowan@gmail.com>
2014-02-09 20:56:28 -0800

    Split MIDI menu stuff into its own screen.

---
+++ src/midi_screen.py
@@ -0,0 +1,52 @@
+from kivy.adapters.dictadapter import DictAdapter
+from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.listview import ListItemButton, ListView
+from kivy.uix.screenmanager import Screen
+
+from keyboard import Keyboard
+
+from kivy.lang import Builder
+Builder.load_string("""
+""")
+
+
+def midi_dict_args_converter(row_index, device_name):
+    return {
+        'index': row_index,
+        'text': device_name,
+        'size_hint_y': None,
+        'height': 25
+    }
+
+
+class MidiScreen(Screen):
+    def __init__(self, **kw):
+        super(MidiScreen, self).__init__(**kw)
+        root = BoxLayout(orientation='vertical',
+                         size_hint=(1.0, 1.0),
+                         padding=10,
+                         spacing=10)
+        kb = Keyboard(anchor_x='center', anchor_y='center',
+                      size_hint=(None, None),
+                      size=(200, 100))
+
+        self.add_widget(root)
+        # set up the midi device list
+        midi_list_layout = AnchorLayout()
+        root.add_widget(midi_list_layout)
+        midi_data = kb.midi_in.available_ports()
+        dict_adapter = DictAdapter(sorted_keys=sorted(midi_data.keys()),
+                                   data=midi_data,
+                                   args_converter=midi_dict_args_converter,
+                                   cls=ListItemButton,
+                                   selection_mode='single',
+                                   allow_empty_selection=False)
+        midi_list_layout.add_widget(ListView(adapter=dict_adapter,
+                                             size_hint=(0.5, 0.8)))
+
+        # set up the keyboard layout
+        keyboard_layout = AnchorLayout()
+        root.add_widget(keyboard_layout)
+        keyboard_layout.add_widget(kb)
+        dict_adapter.bind(on_selection_change=kb.midi_port_changed)
--- src/tonepoem.py
+++ src/tonepoem.py
@@ -1,53 +1,14 @@
 from kivy.app import App
-from kivy.adapters.dictadapter import DictAdapter
-from kivy.uix.listview import ListItemButton, ListView
-from kivy.uix.anchorlayout import AnchorLayout
-from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.screenmanager import ScreenManager
 
-from keyboard import Keyboard
+from midi_screen import MidiScreen
 
 
-def midi_dict_args_converter(row_index, device_name):
-    return {
-        'index': row_index,
-        'text': device_name,
-        'height': 25
-    }
-
-
-# Make a menu to get the right MIDI device
 class TonePoemApp(App):
     def build(self):
-        root = BoxLayout(orientation='vertical',
-                         size_hint=(1.0, 1.0),
-                         padding=10,
-                         spacing=10)
-        kb = Keyboard(anchor_x='center', anchor_y='center',
-                      size_hint=(None, None),
-                      size=(200, 100))
-
-        # set up the midi device list
-        midi_list_layout = AnchorLayout()
-        root.add_widget(midi_list_layout)
-        midi_data = kb.midi_in.available_ports()
-        dict_adapter = DictAdapter(sorted_keys=sorted(midi_data.keys()),
-                                   data=midi_data,
-                                   args_converter=midi_dict_args_converter,
-                                   cls=ListItemButton,
-                                   selection_mode='single',
-                                   allow_empty_selection=False)
-        midi_list_layout.add_widget(ListView(adapter=dict_adapter,
-                                             size_hint=(0.5, 0.8)))
-
-        # set up the keyboard layout
-        keyboard_layout = AnchorLayout()
-        root.add_widget(keyboard_layout)
-        keyboard_layout.add_widget(kb)
-        dict_adapter.bind(on_selection_change=kb.midi_port_changed)
-        print(kb.size)
-        print(kb.to_window(*kb.pos))
-        print(kb.children)
-        return root
+        sm = ScreenManager()
+        sm.add_widget(MidiScreen(name='midi'))
+        return sm
 
 
 if __name__ == '__main__':
