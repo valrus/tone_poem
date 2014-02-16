b3661dc12e81d62cccb6d7100fc8f574b0849e8d
Ian McCowan <imccowan@gmail.com>
2014-02-15 18:07:46 -0800

    Move layout code into kv file.

--- src/battle_screen.py
+++ src/battle_screen.py
@@ -10,20 +10,6 @@ from musicplayer import MusicPlayer
 class BattleScreen(Screen):
     def __init__(self, **kw):
         super(BattleScreen, self).__init__(**kw)
-        root = BoxLayout(orientation='vertical',
-                         size_hint=(1.0, 1.0),
-                         padding=10,
-                         spacing=10)
-        battleArea = RelativeLayout(size_hint=(1.0, 0.6))
-        root.add_widget(battleArea)
-        keyboardArea = AnchorLayout(size_hint=(1.0, 0.1))
-        kb = MidiKeyboard(anchor_x='left', anchor_y='center',
-                          size_hint=(1.0, 1.0))
-        keyboardArea.add_widget(kb)
-        root.add_widget(keyboardArea)
-        infoArea = BoxLayout(size_hint=(1.0, 0.3))
-        root.add_widget(infoArea)
-        self.add_widget(root)
 
         self.music = MusicPlayer()
 
--- src/midi_screen.py
+++ src/midi_screen.py
@@ -19,29 +19,17 @@ def midi_dict_args_converter(row_index, device_name):
 class MidiScreen(Screen):
     def __init__(self, **kw):
         super(MidiScreen, self).__init__(**kw)
-        root = BoxLayout(orientation='vertical',
-                         size_hint=(1.0, 1.0),
-                         padding=10,
-                         spacing=10)
-        kb = MidiKeyboard(anchor_x='center', anchor_y='center',
-                          size_hint=(0.5, 0.5))
 
-        self.add_widget(root)
         # set up the midi device list
-        midi_list_layout = AnchorLayout()
-        root.add_widget(midi_list_layout)
-        midi_data = kb.midi_in.available_ports()
+        midi_data = self.ids.kb.midi_in.available_ports()
         dict_adapter = DictAdapter(sorted_keys=sorted(midi_data.keys()),
                                    data=midi_data,
                                    args_converter=midi_dict_args_converter,
                                    cls=ListItemButton,
                                    selection_mode='single',
                                    allow_empty_selection=False)
-        midi_list_layout.add_widget(ListView(adapter=dict_adapter,
-                                             size_hint=(0.5, 0.8)))
+        self.ids.midi_list_layout.add_widget(ListView(adapter=dict_adapter,
+                                                      size_hint=(0.5, 0.8)))
 
         # set up the keyboard layout
-        keyboard_layout = AnchorLayout()
-        root.add_widget(keyboard_layout)
-        keyboard_layout.add_widget(kb)
-        dict_adapter.bind(on_selection_change=kb.midi_port_changed)
+        dict_adapter.bind(on_selection_change=self.ids.kb.midi_port_changed)
