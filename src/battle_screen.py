fc729da3beee5538ad0fb8c25b80a52ec964ad50
Ian McCowan <imccowan@gmail.com>
2014-02-16 21:35:50 -0800

    Put some beasties in to face off against the party.

--- src/battle_screen.py
+++ src/battle_screen.py
@@ -3,25 +3,41 @@ from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.screenmanager import Screen
 
 from musicplayer import MusicPlayer
+from party import BeastieParty
+from tools import y_iterator
 
 
-class PCWidget(AnchorLayout):
+class CreatureWidget(AnchorLayout):
     image_source = StringProperty(None)
 
     def __init__(self, atlas, **kw):
-        super(PCWidget, self).__init__(**kw)
+        super(CreatureWidget, self).__init__(**kw)
         self.image_source = atlas
 
 
 class BattleScreen(Screen):
     def __init__(self, **kw):
         self.party = kw.pop('party')
+        self.beasties = BeastieParty()
         super(BattleScreen, self).__init__(**kw)
 
         for pc in self.party.members:
             self.ids.player_area.add_widget(
-                PCWidget(pc.atlas)
+                CreatureWidget(pc.atlas)
             )
+
+        self.creature_widgets = []
+        for b, y_loc in zip(self.beasties.members, y_iterator()):
+            self.creature_widgets.append(
+                CreatureWidget(b.atlas)
+            )
+            self.ids.beastie_area.add_widget(
+                self.creature_widgets[-1]
+            )
+
+        print([cw.anchor_y for cw in self.creature_widgets])
+        print([cw.pos for cw in self.creature_widgets])
+        print([cw.size for cw in self.creature_widgets])
 
         self.music = MusicPlayer()
 
--- src/party.py
+++ src/party.py
@@ -2,10 +2,26 @@ from creature import PlayerCharacter, Beastie
 
 
 class Party(object):
+    pass
+
+
+class PlayerParty(Party):
     def __init__(self):
         self.members = [
-            PlayerCharacter('valrus',
+            PlayerCharacter('',
                             'sprites/walrus'),
-            PlayerCharacter('turtle',
+            PlayerCharacter('Sally',
                             'sprites/turtle')
+        ]
+
+
+class BeastieParty(Party):
+    def __init__(self):
+        self.members = [
+            Beastie('Timothy',
+                    'sprites/landeel'),
+            Beastie('Albert',
+                    'sprites/pinkelephant'),
+            Beastie('Nathan',
+                    'sprites/landeel'),
         ]
