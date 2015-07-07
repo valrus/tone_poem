17a2d4d06d7000bcadf2e23f4065a77bf65a8313
valrus <imccowan@gmail.com>
2015-07-06 21:03:47 -0700

    Indentation.

--- src/multiquad.glsl
+++ src/multiquad.glsl
@@ -26,9 +26,9 @@ void main (void) {
   float a_sin = sin(rot);
   float a_cos = cos(rot);
   mat4 rot_mat = mat4(a_cos, -a_sin, 0.0, 0.0,
-                    a_sin, a_cos, 0.0, 0.0,
-                    0.0, 0.0, 1.0, 0.0,
-                    0.0, 0.0, 0.0, 1.0 );
+                      a_sin, a_cos, 0.0, 0.0,
+                      0.0, 0.0, 1.0, 0.0,
+                      0.0, 0.0, 0.0, 1.0 );
   mat4 trans_mat = mat4(1.0, 0.0, 0.0, vCenter.x,
                         0.0, 1.0, 0.0, vCenter.y,
                         0.0, 0.0, 1.0, 0.0,
