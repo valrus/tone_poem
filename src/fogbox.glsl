5464ee3ea9ca9cb7881310097b147f6dfa8adebe
valrus <imccowan@gmail.com>
2016-01-26 22:41:45 -0800

    Pass in edges instead of corners, also accept edge darknesses (but don't use them yet)

--- src/fogbox.glsl
+++ src/fogbox.glsl
@@ -39,16 +39,17 @@ varying vec4 frag_color;
 
 /* uniform texture samplers */
 // uniform sampler2D  texture0;
-uniform vec2       corners[12];
+uniform vec4       edges[12];
+uniform float      darknesses[12];
 uniform float      fNumSides;
 uniform vec2       centerCoords;
 uniform vec2       resolution;
 
-float pointToLine(vec2 p0, vec2 p1, vec2 p2) {
+float pointToLine(vec2 p0, vec4 seg) {
     /* https://en.wikipedia.org/wiki/Distance_from_a_point_to_a_line#Line_defined_by_two_points */
-    float rise = p2.y - p1.y;
-    float run = p2.x - p1.x;
-    return abs(rise * p0.x - run * p0.y + p2.x * p1.y - p2.y * p1.x) / sqrt(rise * rise + run * run);
+    float rise = seg.q - seg.t;
+    float run = seg.p - seg.s;
+    return abs(rise * p0.x - run * p0.y + seg.p * seg.t - seg.q * seg.s) / sqrt(rise * rise + run * run);
 }
 
 void main(void) {
@@ -57,11 +58,12 @@ void main(void) {
     float centerDistance = distance(cPos, centerCoords);
 
     // calculate the distance from the point to the closest edge
-    float edgeDistance = pointToLine(cPos, corners[0], corners[numSides - 1]);
-    for (int i = 0; i < numSides - 1; i++) {
-        float dist = pointToLine(cPos, corners[i], corners[i + 1]);
+    float edgeDistance = 1.0 / 0.0;
+    // omitting an edge from this list will make it light all the way to the edge
+    for (int i = 0; i < numSides; i++) {
+        float dist = pointToLine(cPos, edges[i]);
         if (dist < edgeDistance) {
-            edgeDistance = dist;
+            edgeDistance = dist * darknesses[i];
         }
     }
 
@@ -69,5 +71,5 @@ void main(void) {
     gl_FragColor = vec4(frag_color.r,
                         frag_color.g,
                         frag_color.b,
-                        min(frag_color.a + alpha, 1.0));
+                        min(frag_color.a + alpha * alpha, 1.0));
 }
